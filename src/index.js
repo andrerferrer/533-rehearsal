e1ee5a9c69f875ac10c176805cc0126026f983e1
andre ferrer <andrerferrer@hotmail.com>
2021-03-27 07:11:41 -0300

    Finish livecode

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("Hello from src/index.js!");
+// 1. select the element
+const btn = document.querySelector('#click-me');
+// 2. add event listener
+btn.addEventListener('click', (event) => {
+  // 3. perform the behavior
+  event.currentTarget.innerText = 'Loading';
+});
